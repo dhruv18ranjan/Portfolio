1f9a882b5532c8c09bb761137a3660be1a26cc3f
dhruv <dhruv18ranjan@gmail.com>
2023-02-18 23:27:18 +0530

    social links

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -22,10 +22,10 @@ const Landing = ({setSelectedPage}) => {
             ):(
                 <div>
                     <img src="assets\profile-image.png" alt="profile" className='hover:filter hover:saturate-200 
-                    transition duration-500 z-10 w-full max-w-[400px] md:max-w-[400px] ' />
+                    transition duration-500 z-10 w-full max-w-[300px] md:max-w-[400px] ' />
                 </div>
             )}
-
+            </div>
                 {/* main */}
                 <div className='z-30 basis-2/5 mt-12 md:mt-32'>
                 {/* heading */}
@@ -45,10 +45,57 @@ const Landing = ({setSelectedPage}) => {
                             Ranjan
                         </span>
                     </p>
+                    <p className="mt-10 mb-7 text-sm text-center  md:text-start ">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus in corrupti dolore 
+                        molestiae sed molestias dolorum. Vel, aspernatur qui cupiditate, id, repellendus voluptatem
+                         magni aliquid porro quasi laudantium asperiores tempore!
+                    </p>
                 </motion.div>
+                {/* call to actions */}
+                <motion.div 
+                    className='flex mt-5 justify-center md:justify-start'
+                    initial="hidden" whileInView="visible"
+                    viewport={{once:true,amount:0.5}}
+                    transition={{delay:0.2,duration:0.5}}
+                    variants={{hidden:{opacity:0 , x:-50},
+                                visible:{opacity:1, x:0}
+                             }}
+                >
+                    <AnchorLink 
+                        onClick={()=> setSelectedPage("contact")}
+                        className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+                         hover:bg-blue hover:text-white transition duration-500"
+                        href='#contact'
+                    >
+                         Contact Me
+                    </AnchorLink>
+                    <AnchorLink 
+                        onClick={()=> setSelectedPage("contact")}
+                        className=" rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5 "
+                    >
+                        <div className='bg-deep-blue hover:text-red transition duration-500 
+                        flex items-center h-full w-full justify-center font-playfair px-10'>
+
+                         Let's Talk !
+                        </div>
+                    </AnchorLink>
+                </motion.div>
+
+                <motion.div 
+                    className='flex mt-5 justify-center md:justify-start'
+                    initial="hidden" whileInView="visible"
+                    viewport={{once:true,amount:0.5}}
+                    transition={{delay:0.4,duration:0.5}}
+                    variants={{hidden:{opacity:0 , x:-50},
+                                visible:{opacity:1, x:0}
+                             }}
+                >
+
+                </motion.div>
+
+
                 </div>
 
-        </div>
     </section>
   )
 }
---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SocialMediaIcons = () => {
+  return (
+    <div className='flex justify-center md:justify-start my-10 gap-10'>
+        
+    </div>
+  )
+}
