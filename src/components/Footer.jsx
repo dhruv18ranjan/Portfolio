b390e120b55b885e46cee32444460e8835b65cdf
dhruv <dhruv18ranjan@gmail.com>
2023-02-24 21:06:06 +0530

    m

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { SocialMediaIcons } from "./SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer className="h-46 bg-blue w-[100%] pt-8 lg:w-[100%] sm:w-[100%] ss:w-[100%] md:w-[100%] xs:w-[100%]">
+    <footer className="h-46 bg-blue w-full flex xs:w-[100vw] ">
       <div className="w-full sm:w-full xs:w-full mx-auto">
         <SocialMediaIcons />
         <div className="md:flex justify-between  md:justify-between text-center ">
